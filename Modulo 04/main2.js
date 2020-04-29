16b6014422f06cd898a48bdfc4451268d82ba5e2
Allyson Reis <allysonreis1@gmail.com>
2020-04-29 16:43:20 -0300

    Criando função search para fazer uma requisição ajax

--- Modulo 04/main2.js
+++ Modulo 04/main2.js
@@ -1,3 +1,4 @@
+let name = '';
 var bodyElement = document.querySelector('body');
 
 var input = document.createElement('input');
@@ -5,7 +6,14 @@ input.setAttribute('type', 'text');
 input.setAttribute('placeholder', 'Digite o usuário')
 
 var button = document.createElement('button');
+button.setAttribute('onclick', 'search('+ input.value +')');
 button.innerText = 'Buscar';
 
 bodyElement.appendChild(input);
 bodyElement.appendChild(button);
+
+function search (name) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.github.com/users/'+ name +'/repos');
+  xhr.send(null);
+}

adf8c438ba648095f6eb9e07048abadfa7e8cc5c
Allyson Reis <allysonreis1@gmail.com>
2020-04-29 16:06:08 -0300

    Usando arrow function para chamar o resolve e reject

--- Modulo 04/main.js
+++ Modulo 04/main.js
@@ -1,15 +1,17 @@
 function checaIdade(idade) {
   // Retornar uma promise
   return new Promise(function(resolve, reject){
-    setTimeout(idade > 18 ? resolve : reject, 2000);
+    setTimeout(()=>{
+      idade > 18 ? resolve() : reject()
+    }, 2000 );
   });
  }
  
  checaIdade(25)
   .then(function() {
-  console.log("Maior que 18");
+    console.log("Maior que 18");
   })
   .catch(function() {
-  console.log("Menor que 18");
+    console.log("Menor que 18");
   });
  

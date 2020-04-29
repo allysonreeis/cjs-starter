c4bd0bd487771a24d10b73581e36c68d8f181e19
Allyson Reis <allysonreis1@gmail.com>
2020-04-29 17:47:48 -0300

    Removendo a criação de elementos html do js e colocando no proprio html

--- Modulo 04/main2.js
+++ Modulo 04/main2.js
@@ -1,18 +1,10 @@
-let name = '';
 var bodyElement = document.querySelector('body');
 
-var input = document.createElement('input');
-input.setAttribute('type', 'text');
-input.setAttribute('placeholder', 'Digite o usuário')
+var button = document.querySelector('button');
+button.setAttribute('onclick', `search()`);
 
-var button = document.createElement('button');
-button.setAttribute('onclick', 'search('+ input.value +')');
-button.innerText = 'Buscar';
-
-bodyElement.appendChild(input);
-bodyElement.appendChild(button);
-
-function search (name) {
+function search () {
+  var name = document.querySelector('input').value;
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.github.com/users/'+ name +'/repos');
   xhr.send(null);

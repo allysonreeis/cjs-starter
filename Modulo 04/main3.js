92f87fc331e8c8af376930ff40a3bab972a6c29e
Allyson Reis <allysonreis1@gmail.com>
2020-04-30 14:56:37 -0300

    Adicionando then e catch. Melhorando renderizacao

--- Modulo 04/main3.js
+++ Modulo 04/main3.js
@@ -5,22 +5,36 @@ var button = document.querySelector('button');
 button.setAttribute('onclick', 'search()');
 
 function search () {
-  var name = document.querySelector('input').value;
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.github.com/users/'+ name +'/repos');
-  xhr.send(null);
+  return new Promise(
+    function(resolve, reject){
+    
+      var name = document.querySelector('input').value;
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://api.github.com/users/'+ name +'/repos');
+      xhr.send(null);
 
-  xhr.onreadystatechange = function(){
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var obj = JSON.parse(xhr.responseText);
-        renderRepositories(obj);
+      xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            console.log('Ok');
+            var obj = JSON.parse(xhr.responseText);
+            renderRepositories(obj);
+          } else {
+            reject('Erro');
+          }
+        }
       }
-    }
-  }
+  })
+  .then(()=>{
+    console.log('Ok!');
+  })
+  .catch(()=>{
+    console.warn('erro 404 - Usuário não encontrado');
+  });
 }
 
 function renderRepositories(repositories) {
+  ulElement.innerHTML = "";
   repositories.forEach(repository => {
     var liElement = document.createElement('li');
     liElement.innerText = repository.name;
